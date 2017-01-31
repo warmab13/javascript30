72aa77bded9527c01e8da47e339831aaedaddbb3
warmab13 <alonsosalcido13@gmail.com>
2017-01-31 09:57:39 -0700

    Drum Kit finished

---
+++ drum-js/js/drumjs.js
@@ -0,0 +1,22 @@
+function playSound(e){
+  //console.log(e.keyCode);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  //console.log(audio);
+  if(!audio) return;//stop the function from running all together
+  audio.currentTime = 0;//rewind to the start
+  audio.play();
+  key.classList.add('playing');
+
+  const keys = document.querySelectorAll('.key');
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
+  function removeTransition(e){
+  if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+  }
+
+}
+
+window.addEventListener('keydown', playSound);
